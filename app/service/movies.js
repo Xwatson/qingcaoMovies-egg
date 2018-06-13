f836f5d96dd51211ce244f3d515cd8585ee8e7d0
xwatson <xuwusheng@hotmail.com>
2018-06-14 00:26:03 +0800

    修改movie创建bug

--- app/service/movies.js
+++ app/service/movies.js
@@ -1,6 +1,7 @@
 'use strict';
 const Service = require('egg').Service;
 const SearchMovie = require('../../util/searchMovie');
+const responseCode = require('../../util/responseCode');
 
 class MovieService extends Service {
   constructor(ctx) {
@@ -49,38 +50,49 @@ class MovieService extends Service {
   }
   async batchCreateTheaters(movies = []) {
     // 查询本次数据库已有的电影
-    const dbIds = movies.map(m => m.id);
-    const existMovies = await this.ctx.model.Movies.findAll({
+    const dbIds = movies.map(m => m.douban_id);
+    let existMovies = await this.ctx.model.Movies.findAll({
+      attributes: [ 'douban_id' ],
       where: {
         douban_id: dbIds,
       },
     });
-    // console.log('已经存在的电影：', existMovies);
     const createMovies = [];
     if (existMovies) {
+      existMovies = existMovies.map(m => m.douban_id);
       movies.forEach(m1 => {
-        if (!existMovies.find(m2 => m1.douban_id === m2.douban_id)) {
+        if (existMovies.indexOf(m1.douban_id) === -1) {
           createMovies.push(m1);
         }
       });
       // console.log('需要插入的电影：', createMovies);
       // 获取电影资源
       let aaqqyMovie = {};
+      const searchMovie = new SearchMovie();
       for (const key in createMovies) {
         try {
-          const searchMovie = new SearchMovie();
           aaqqyMovie = await searchMovie.search(createMovies[key].title, aaqqyMovie.proxy);
-          console.log('搜索完成' + key, aaqqyMovie);
+        } catch (error) {
+          if (error.code === responseCode.proxyError) {
+            console.log('获取代理错误: ', error.message);
+            return;
+          }
+          console.log(`电影《${createMovies[key].title}》未收录`, error);
+        }
+        if (aaqqyMovie.movie) {
+          createMovies[key].aayyq_id = parseInt(aaqqyMovie.movie.aayyq_id);
           createMovies[key].clarity = aaqqyMovie.movie.status;
           createMovies[key].area = aaqqyMovie.movie.area;
           createMovies[key].plot = aaqqyMovie.movie.plot;
           createMovies[key].player_url = aaqqyMovie.movie.player_url;
           createMovies[key].update_time = aaqqyMovie.movie.update_time;
-        } catch (error) {
-          console.log(`电影《${createMovies[key].title}》未收录`, error);
+        }
+        const _create = await this.create(createMovies[key]);
+        if (_create) {
+          console.log(`插入电影《${_create.title}》成功`);
         }
       }
-      // console.log('搜索结束：', createMovies);
+      console.log('搜索结束');
     }
   }
 }
