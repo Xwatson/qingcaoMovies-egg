f0969bb475367d9257f762d46156a3314c1d744a
xwatson <xwatson@foxmail.com>
2018-07-05 17:47:12 +0800

    修改获取电影定时任务间隔

--- app/schedule/update_newMovies.js
+++ app/schedule/update_newMovies.js
@@ -10,7 +10,7 @@ class UpdateCache extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      interval: '24h', // 1 分钟间隔
+      interval: '2h',
       type: 'all', // 指定所有的 worker 都需要执行
     };
   }
