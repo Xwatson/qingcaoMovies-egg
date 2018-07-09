c0250af069b65868c819ca7a6e11a6b2e53d4de1
xwatson <xwatson@foxmail.com>
2018-07-09 12:12:51 +0800

    取消获取豆瓣正在上映任务

--- app/schedule/update_douBan.js
+++ app/schedule/update_douBan.js
@@ -14,7 +14,7 @@ class UpdateDouBan extends Subscription {
 
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    await this.getInTheaters();
+    // await this.getInTheaters();
   }
   async getInTheaters() {
     const iTheaters = await startTnTheaters(this);
