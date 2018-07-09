84cce00da30cae6636c8c358544b044745bb93fe
xwatson <xwatson@foxmail.com>
2018-07-09 19:19:07 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = app => {
+  app.appTitle = '青草影院';
   app.beforeStart(async () => {
     // 保证应用启动监听端口前数据已经准备好了
     // 后续数据的更新由定时任务自动触发
--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const movies = await this.ctx.service.movies.getMovies({});
-    await this.ctx.render('home.ejs', { title: '青草影院', movies });
+    await this.ctx.render('home.ejs', { title: this.app.appTitle, movies });
   }
 }
 
